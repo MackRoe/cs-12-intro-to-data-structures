c91146cfc07ad43a63ee89e94f476414f738a602
MackRoe <elaine.music@students.makeschool.com>
2020-01-26 13:43:21 -0800

    create append_string helper function

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,4 +1,10 @@
 from random import randint
+from sys import sys
+
+def get_string_length():
+    '''gets the length of string to output from console'''
+    string_length = sys.argv[1:]
+    return string_length
 
 
 def read_file():
@@ -11,21 +17,30 @@ def read_file():
     return my_words
 
 
-def select_words(my_words):
+def select_word(my_words):
     '''select a random set of words and store in a data type'''
     rand_index = randint(0, len(my_words) - 1)
 
-    random_word_string = my_words[rand_index]
-    return random_word_string
+    random_word = my_words[rand_index]
+    return random_word
+
+
+def append_string(string_length):
+    '''compile string helper function'''
+    random_string = ""
+    while string_length < 0:
+        random_string = select_word()
+
+    pass
 
 
 def output_string():
     '''output the string'''
-    print(select_words())
+    print(select_word())
 
 # hint: look at rearrange() and random_python_quote()
 
 
 read_file()
-select_words()
+# select_word()
 output_string()
