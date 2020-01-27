4d3a5f66a69ef4809388c1edfc6d13ced152a8e7
MackRoe <elaine.music@students.makeschool.com>
2020-01-27 13:21:39 -0800

    dictionary_words.py now prints one random word

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,25 +1,22 @@
 from random import randint
-from sys import sys
-
-def get_string_length():
-    '''gets the length of string to output from console'''
-    string_length = sys.argv[1:]
-    return string_length
+import sys as sys
 
 
-def read_file():
-    '''reads the words file into memory'''
-    filename = "my_massive_list.txt"
+'''gets the length of string to output from console'''
+string_length = int(sys.argv[1])
 
-    my_massive_list = open("url/share/dict/words", 'r')
-    my_words = my_massive_list.readlines()
-    my_massive_list.close()
-    return my_words
+
+'''reads the words file into memory'''
+filename = "/usr/share/dict/words"
+
+my_massive_list = open(filename, 'r')
+my_words = my_massive_list.readlines()
+# my_massive_list.close()
 
 
 def select_word(my_words):
     '''select a random set of words and store in a data type'''
-    rand_index = randint(0, len(my_words) - 1)
+    rand_index = randint(1, len(my_words) - 1)
 
     random_word = my_words[rand_index]
     return random_word
@@ -29,18 +26,16 @@ def append_string(string_length):
     '''compile string helper function'''
     random_string = ""
     while string_length < 0:
-        random_string = select_word()
-
+        random_string = random_string + select_word()
+        string_length -= 1
     pass
 
 
 def output_string():
     '''output the string'''
-    print(select_word())
+    print(select_word(my_words))
 
 # hint: look at rearrange() and random_python_quote()
 
-
-read_file()
 # select_word()
 output_string()
