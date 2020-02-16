6ab858c5e97b673127cb75ae49681ddb6e591638
MackRoe <elaine.music@students.makeschool.com>
2020-02-16 14:58:38 -0800

    restore weighted_sample.py and word_frequency.py. create new folder Tweet_Generator. move relevent files into new folder

---
+++ Tweet_Generator/weighted_sample.py
@@ -0,0 +1,44 @@
+from random import randint
+
+text = 'one fish two fish red fish blue fish'
+word_counts = {'one': 1, 'fish': 4, 'two': 1,
+               'red': 1, 'blue': 1}
+
+
+def sample_by_frequency(histogram):
+
+    # TODO: select a word based on frequency
+    # how can we sample words using observed frequencies?
+    # randint() for index
+    loop_count = 0
+
+    while loop_count < len(histogram):
+        new_list = []
+        for word, word_count in histogram.items():
+            count = 0
+            while word_count > count:
+                new_list.append(word)
+                count += 1
+        loop_count += 1
+
+    # print("New List:")
+    # for word in new_list:
+    #     print(word)
+
+    index = randint(0, len(new_list) - 1)
+    # print("Random Index is " + str(index))
+
+#     -- in class example --
+# random_index = randint(0, sum(histogram.values()))
+#  start = 0
+#  for k,v in histogram.items():
+#    end = start + count
+#    if random_index >= start and random_index <= end
+#       return word
+#     else:
+#       start = end ---sorta?---
+
+    return new_list[index]
+
+
+print('Random Word: ' + sample_by_frequency(word_counts))
---
+++ Tweet_Generator/word_frequency.py
@@ -0,0 +1,58 @@
+import sys as sys
+
+'''gets the word for which frequency of occurance value is sought'''
+'''search_word = str(sys.argv[1])
+
+filename = "transient.txt"
+lines = open(filename, "r").readlines()
+transient_txt_words = []
+
+
+for line in lines:
+    wordslist = line.split(' ')
+    for word in wordslist:
+        word = word.strip(' . , ;" \n _')
+        transient_txt_words.append(word)
+# print(transient_txt_words)
+# input()
+
+# print('transient_txt_words contains: ')
+# for word in transient_txt_words:
+#     print(word)
+
+my_histogram = {}'''
+
+
+def histogram(transient_txt_words):
+    '''function which takes a source_text argument (can be either a filename or
+    the contents of the file as a string, your choice) and return a histogram
+    data structure that stores each unique word along with the number of times
+    the word appears in the source text'''
+    my_histogram = {}
+    for word in transient_txt_words:
+        word = word.strip(' . , ; " \n _')
+        # print(word)
+
+        my_histogram[word] = my_histogram.get(word, 0)+1
+    # print(my_histogram)  # print to test
+    return my_histogram
+
+
+def unique_words(any_histogram):
+    '''function that takes a histogram argument and returns the total count of
+    unique words'''
+    number_of_unique_words = len(any_histogram)
+    return number_of_unique_words
+
+
+def frequency(word, any_histogram):
+    '''function that takes a word and histogram argument and returns the number
+    of times that word appears in a text.'''
+    frequency_of_word = any_histogram[word]
+    return frequency_of_word
+
+
+# histogram()
+
+# print('Histogram contains ' + str(unique_words(my_histogram)) + ' unique words')
+# print(search_word + ' appears ' + str(frequency(search_word, my_histogram)) + ' times.')
